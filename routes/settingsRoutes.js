5fce0cb2d8cb8aacf2d2ac45dae885dd40f406cd
leviator456-stack <leviator456@gmail.com>
2026-07-16 12:09:24 +0530

    Update settingsRoutes.js

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -3,13 +3,6 @@ import { clearChatHistory } from "../controllers/settingsController.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Settings route working hai."
-  });
-});
-
 router.delete("/clear-chat-history", clearChatHistory);
 
 export default router;
