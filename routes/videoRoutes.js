394c6421e7847c7a61b76917317d3f89d6ca8bee
leviator456-stack <leviator456@gmail.com>
2026-07-21 19:15:55 +0530

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,24 +1,25 @@
 import express from "express";
+
 import {
   generateVideo,
-  getVideoStatus,
+  getVideoStatus
 } from "../controllers/videoController.js";
 
-import { requireActiveSubscription } from "../middleware/subscription.middleware.js";
+import requireVideoSubscription from "../middleware/requireVideoSubscription.js";
 
 const router = express.Router();
 
 // Generate a new AI video
 router.post(
   "/generate",
-  requireActiveSubscription("video"),
+  requireVideoSubscription,
   generateVideo
 );
 
 // Check video generation status
 router.get(
   "/status/:taskId",
-  requireActiveSubscription("video"),
+  requireVideoSubscription,
   getVideoStatus
 );
 
